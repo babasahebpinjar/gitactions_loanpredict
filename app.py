f58a9a0859cab10db88d1efc8e286c4c6ced27d1
babasahebpinjar <babasahebpinjar@gmail.com>
2023-12-30 18:28:36 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, Request
+from flask import Flask, request, Response, jsonify
+import pickle
+import uvicorn
+import json
+from fastapi.responses import JSONResponse
+## loading the model
+model_pickle = open("./artifacts/classifier.pkl", 'rb')
+clf = pickle.load(model_pickle)
+
+
+app = FastAPI()
+
+
+@app.post("/predict")
+async def home(request: Request):
+    return "Hello World"
+
+
+@app.post('/predict1',)
+async def prediction(requestVal:str):
+    # Pre-processing user input
+    # Pre-processing user input
+    print("-----",requestVal)
+    requestVal = json.loads(requestVal)
+    print(type(requestVal))
+    loan_req = requestVal #.get_json()
+    print(loan_req) 
+
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+ 
+    if loan_req['Married'] == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+ 
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1  
+    
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount'] / 1000
+ 
+    # Making predictions 
+    prediction = clf.predict( 
+        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+     
+    if prediction == 0:
+        pred = 'Rejected'
+    else:
+        pred = 'Approved'
+
+    result = {
+        'loan_approval_status': pred
+    }
+
+    return JSONResponse(content=result)
+
+if __name__ == "__main__":
+    
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ tests/test_app.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+from app import app  # Assuming your FastAPI app is in a file named main.py
+import json
+
+client = TestClient(app)
+
+def test_hello_world():
+    response = client.post("/predict")
+    assert response.status_code == 200
+    #print("<<<<<<<<<<<<<<<<<")
+    assert response.json() == "Hello World"
+
+
+def test_prediction():
+    sample_input = {
+        "Gender": "Male",
+        "Married": "Unmarried",
+        "Credit_History": "Unclear Debts",
+        "ApplicantIncome": 5000,
+        "LoanAmount": 150
+    }
+
+    response = client.post("/predict1?requestVal=" + json.dumps(sample_input))
+    result = response.json()
+    assert 'loan_approval_status' in result
+    assert result['loan_approval_status'] in ['Approved', 'Rejected']
+    print(response.content)
+    assert response.status_code == 200
